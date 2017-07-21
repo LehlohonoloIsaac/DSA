7afea5ce2cba5aabc3c4e18af3159a6faf53bdc7
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2017-07-21 15:59:23 +0200

    Add update Stack playground

--- Stack.playground/Contents.swift
+++ Stack.playground/Contents.swift
@@ -2,4 +2,45 @@
 
 import UIKit
 
-var str = "Hello, playground"
+struct Stack<T>{
+    fileprivate var data = [T]()
+    var top:Int
+    
+    init(data: [T]){
+        self.data = data
+        top = -1
+    }
+    
+    mutating func push(value: T){
+        top = top + 1
+        data.append(value)
+    }
+    
+    func isEmpty() -> Bool{
+        return data.isEmpty
+    }
+    
+    func size() -> Int {
+        return data.count
+    }
+    
+    mutating func pop() -> T{
+        guard let value = data.popLast() else {return -1 as! T }
+        top = top - 1
+        return value
+    }
+}
+
+var data:[Int] = [Int]()
+
+var stack = Stack(data: data)
+print("\(stack.isEmpty())")
+stack.push(value: 8)
+stack.push(value: 3)
+stack.push(value: 38)
+print(stack.data)
+print("top = \(stack.top)")
+stack.pop()
+print(stack.data)
+print("top = \(stack.top)")
+print(stack.isEmpty())
